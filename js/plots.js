7e65fcea9420ee5f98f04ce22e439f08fd8d09ee
MeijerW <110025532+MeijerW@users.noreply.github.com>
2026-03-13 12:00:21 +0100

    Add spatialPlot function for RNA and Protein visualization

---
+++ js/plots.js
@@ -0,0 +1,41 @@
+async function spatialPlot(gene){
+
+let rna =
+await loadCSV("RNA_preprocessed.csv")
+
+let prot =
+await loadCSV("Protein_preprocessed.csv")
+
+let rnaGene =
+rna.filter(d =>
+d.Gene.toLowerCase() === gene
+)
+
+let protGene =
+prot.filter(d =>
+d.Gene.toLowerCase() === gene
+)
+
+let trace1={
+x:rnaGene.map(d=>d.group),
+y:rnaGene.map(d=>d["Z-score"]),
+type:"box",
+name:"RNA"
+}
+
+let trace2={
+x:protGene.map(d=>d.group),
+y:protGene.map(d=>d["Z-score"]),
+type:"box",
+name:"Protein"
+}
+
+Plotly.newPlot(
+"plot",
+[trace1,trace2],
+{
+title:"Spatial Expression"
+}
+)
+
+}
